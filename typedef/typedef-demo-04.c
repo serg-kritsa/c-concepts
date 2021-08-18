90600c6efece077f4225bd763a024468cf7ef731
Serg <serg@example.com>
2021-08-18 22:29:09 +0300

    added typedef demo

---
+++ typedef/typedef-demo-04.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#define int_pointer int *
+int_pointer chalk, cheese; // int *chalk, cheese // SUBSTITUTION
+
+
+typedef char *char_ptr;
+char_ptr Bently, Rolls_Royce; // char *Bently, char *Rolls_Royce;
+
